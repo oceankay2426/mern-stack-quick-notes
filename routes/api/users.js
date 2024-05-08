1319d23ab5a6576becdb0ce9e068739ce9136980
oceankay2426 <travelkay24@gmail.com>
2024-05-08 11:10:58 -0500

    MERN-Stack Infrastructure

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+
+// All paths start with '/api/users'
+
+// POST /api/users (create a user - sign up)
+router.post('/', usersCtrl.create);
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
+
+module.exports = router;
